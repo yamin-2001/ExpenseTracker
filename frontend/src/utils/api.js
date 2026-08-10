fc854b3e46e715dc486911c88b10251500005a14
MD. ASHABUL YAMIN <yamin200185@gmail.com>
2026-08-11 02:48:51 +0600

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://expensetracker-9p6r.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
